5110ea5b6ff560d7337d680fa4d6cd67456a992d
Ebun Adebesin <adebesinebunoluwa1@gmail.com>
2024-10-22 13:12:23 -0700

    Initial test

---
+++ src/test/java/edu/neu/mgen/finalproject5100/Finalproject5100ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen.finalproject5100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalproject5100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
